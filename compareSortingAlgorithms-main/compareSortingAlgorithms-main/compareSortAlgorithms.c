0989c1f8a299ba6b2b6baa54903459ab0c20a4d6
bubs5 <67673905+bubs5@users.noreply.github.com>
2023-03-21 21:14:57 -0400

    Update compareSortAlgorithms.c

--- compareSortingAlgorithms-main/compareSortingAlgorithms-main/compareSortAlgorithms.c
+++ compareSortingAlgorithms-main/compareSortingAlgorithms-main/compareSortAlgorithms.c
@@ -8,28 +8,59 @@ int extraMemoryAllocated;
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
-	
+	if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(pData, l, m);
+       	mergeSort(pData, m + 1, r);
+        merge(pData, l, m, r);
+    }
 }
 
 // implement insertion sort
 // extraMemoryAllocated counts bytes of memory allocated
 void insertionSort(int* pData, int n)
 {
-	
+	int i, key, j;
+	for (i = 1; i < n; i++) {
+        key = pData[i];
+        j = i - 1;
+ 
+        while (j >= 0 && pData[j] > key) {
+            pData[j + 1] = pData[j];
+            j = j - 1;
+        }
+        pData[j + 1] = key;
+    }
 }
 
 // implement bubble sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void bubbleSort(int* pData, int n)
 {
-	
+	int i, j;
+    for (i = 0; i < n - 1; i++)
+        for (j = 0; j < n - i - 1; j++)
+            if (pData[j] > pData[j + 1]) {
+                int temp = pData[j];
+                pData[j] = pData[j + 1];
+                pData[j + 1] = temp;
+ }
 }
 
 // implement selection sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void selectionSort(int* pData, int n)
 {
-	
+	int i, j, min_idx;
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+            if (pData[j] < pData[min_idx])
+                min_idx = j;
+        int temp = pData[min_idx];
+        pData[min_idx] = pData[i];
+        pData[i] = temp;
+    }
 }
 
 // parses input file to an integer array
